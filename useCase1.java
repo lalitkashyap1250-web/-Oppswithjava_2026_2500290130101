00ebd063d9028ada02a3f113bb7c8dd9e5c62a4a
Lalit Kumar <lalitkashyap1250@gmail.com>
2026-09-14 13:14:23 +0530

    Add files via upload

---
+++ useCase1.java
@@ -0,0 +1,73 @@
+
+public class useCase1 {
+    public static void main(String[] args) {
+        Book b1 = new Book("Java Basics", "J. Author", 350.0, "ISBN001");
+        Book b2 = new Book("OOP Concepts", "K. Writer", 420.0, "ISBN002");
+ 
+        System.out.println(b1.getTitle() + " by " + b1.getAuthor());
+        System.out.println("Price: " + b2.getPrice());
+        System.out.println("Library: " + Book.libraryName);
+        System.out.println("Total books added: " + Book.bookCount);
+    }
+}
+
+class Book {
+
+    // Private data members
+    private String title;
+    private String author;
+    private double price;
+    private String isbn;
+
+    // Static variable
+    static int bookCount = 0;
+
+    // Final variable
+    static final String libraryName = "Sunrise";
+
+    // Parameterized constructor
+    public Book(String title, String author, double price, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isbn = isbn;
+
+        bookCount++;
+    }
+
+    // Getter and Setter for title
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter and Setter for author
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Getter and Setter for price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter and Setter for isbn
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
